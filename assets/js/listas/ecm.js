bc708266fad6133db786030f655df012a58ae024
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-01-21 12:30:05 -0500

    Add files via upload

---
+++ assets/js/listas/ecm.js
@@ -0,0 +1,18 @@
+                     
+
+fetch('../assets/data/datacursos.json')
+.then(response => response.json() )
+.then(data => {
+    let tr = "";    
+    data.ecm.forEach(function(curso){
+        tr += `            
+               <tr>
+                  <td ><a class=" text-xs text-light" href="${curso.url}"><i class="fab fa-telegram-plane text-success me-2"></i> ${curso.curso}</td>
+                  <td class=" text-xs">${curso.tag}</td>
+                  <td class="text-xs text-center"><a class="  text-success text-bolder" href="${curso.urlPago}">$ ${curso.precio}</a></td> 
+               </tr>
+        `;
+    });
+    document.getElementById('lista-ecm').innerHTML = tr;
+   
+});

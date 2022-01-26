35c4d7b08c73b5cf65de1d2a69fdb21bc0eb294e
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-01-26 16:09:43 -0500

    Update md.js

--- assets/js/listas/md.js
+++ assets/js/listas/md.js
@@ -27,3 +27,14 @@ fetch('../assets/data/datacursos.json')
     document.getElementById('lista-md').innerHTML = div;
    
 });
+
+const d = document;
+function searchFilters(input, selector) {
+    d.addEventListener("keyup", e => {
+        if (e.target.matches(input)){
+              
+            d.querySelectorAll(selector).forEach(el => (el.textContent.toLowerCase().includes(e.target.value))?el.classList.remove("filtro"):el.classList.add("filtro"));
+        }
+    }) 
+}
+searchFilters(".filtro-tarjetas", ".tarjeta");

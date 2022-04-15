87544f7751982645597e39a4d1f5513aa492125e
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-04-15 14:36:58 -0500

    Update listaSuperPack.js

--- assets/js/listaSuperPack.js
+++ assets/js/listaSuperPack.js
@@ -9,7 +9,12 @@ fetch('./assets/data/superPack.json')
     let li = "";   
       data.forEach(function(curso){
         li += `
-        <small><li class="d-flex justify-content-between"><a href="https://api.whatsapp.com/send/?phone=%2B573242793938&text=Hola%2C+Master.+Quiero+la+Promo+del+Super+Pack+de+Desarrollo+Web+por+37.000" class="w-50 border-bottom py-1">${curso.curso}</a> <span class="rounded px-1 w-50 text-center border-bottom"><small>${curso.lenguaje}</small></span></li></small>
+        <small><li class="d-flex justify-content-between">
+        
+          <a href="https://api.whatsapp.com/send/?phone=%2B573242793938&text=Hola%2C+Master.+Quiero+la+Promo+del+Super+Pack+de+Desarrollo+Web+por+37.000" class="w-70 border-bottom py-1"><span class="rounded px-1  text-center "><img class=" shadow rounded-2" src="/assets/img/icons/${curso.main}.png" alt="${curso.main}" title="${curso.main}"</span>  &nbsp;&nbsp; ${curso.curso}</a>
+          <span class="rounded px-1 w-30 text-left border-bottom"><small>${curso.lenguaje}</small></span>
+          
+        </li></small>
         `;
       });
     document.getElementById('listaTodos').innerHTML = li;

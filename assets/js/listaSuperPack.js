f7206c0dd4473f6e3f73e28e98f1ee51f023253a
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-04-15 14:57:10 -0500

    Update listaSuperPack.js

--- assets/js/listaSuperPack.js
+++ assets/js/listaSuperPack.js
@@ -10,9 +10,13 @@ fetch('./assets/data/superPack.json')
       data.forEach(function(curso){
         li += `
         <small><li class="d-flex justify-content-between">
-        
-          <a href="https://api.whatsapp.com/send/?phone=%2B573242793938&text=Hola%2C+Master.+Quiero+la+Promo+del+Super+Pack+de+Desarrollo+Web+por+37.000" class="w-70 border-bottom py-1"><span class="rounded px-1  text-center "><img class=" shadow rounded-2" src="/assets/img/icons/${curso.main}.png" alt="${curso.main}" title="${curso.main}"</span>  &nbsp;&nbsp; ${curso.curso}</a>
+          <div class="w-70 border-bottom py-1 d-flex">
+            <div><img class=" shadow rounded-2" src="/assets/img/icons/${curso.main}.png" alt="${curso.main}" title="${curso.main}"></div>
+            <div class="ms-2"><a href="https://api.whatsapp.com/send/?phone=%2B573242793938&text=Hola%2C+Master.+Quiero+la+Promo+del+Super+Pack+de+Desarrollo+Web+por+37.000" >${curso.curso}</a></div>
+          </div>
           <span class="rounded px-1 w-30 text-left border-bottom"><small>${curso.lenguaje}</small></span>
+          
+          <span class="rounded px-1 w-30 text-center border-bottom"><small>${curso.nivel}</small></span>
           
         </li></small>
         `;

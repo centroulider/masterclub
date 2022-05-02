32290b3c8c55cef0b84070871572acc93fb789e1
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-05-02 13:26:32 -0500

    Update listaSuperPack.js

--- assets/js/listaSuperPack.js
+++ assets/js/listaSuperPack.js
@@ -9,17 +9,18 @@ fetch('./assets/data/superPack.json')
     let li = "";   
       data.forEach(function(curso){
         li += `
-        <small><li class="d-flex justify-content-between">
+        <small class="tarjeta">
+        <li class="d-flex justify-content-between " ${curso.Plataforma} ${curso.Profesor}>
           <div class="w-70 border-bottom py-1 d-flex">
             <div><img class=" shadow rounded-2" src="/assets/img/icons/${curso.main}.png" alt="${curso.main}" title="${curso.main}"></div>
             <div class="ms-2"><a href="https://api.whatsapp.com/send/?phone=%2B573242793938&text=Hola%2C+Master.+Quiero+la+Promo+del+Super+Pack+de+Desarrollo+Web+por+37.000" >${curso.curso}</a></div>
           </div>
-          <span class="rounded px-1 w-30 text-left border-bottom"><small>${curso.lenguaje}</small></span>
+          <span class="rounded px-1 w-30 text-left border-bottom"><small>${curso.lenguaje}<span class="opacity-0 miniText"> ${curso.Plataforma}</span></small></span>
           
           <span class="rounded px-1 w-30 text-center border-bottom"><small>${curso.nivel}</small></span>
-          <span class="rounded px-1 w-30 text-center border-bottom"><small>${curso.duracion} Hs</small></span>
-          
-        </li></small>
+          <span class="rounded px-1 w-30 text-center border-bottom"><small>${curso.duracion} Hs<span class="opacity-2 miniText"> ${curso.Profesor}</span></small><span></span></span>
+        </li>
+        </small>
         `;
       });
     document.getElementById('listaTodos').innerHTML = li;
@@ -40,6 +41,8 @@ fetch('./assets/data/superPack.json')
      document.getElementById('total_horas').innerHTML = totalHoras;
     
 });
+
+
 
 // Filtro de cursos
 

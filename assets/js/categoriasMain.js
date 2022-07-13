18c2f6034513c631759b2cd9f00c6ccab95a2d34
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-07-13 02:33:57 -0500

    Update categoriasMain.js

--- assets/js/categoriasMain.js
+++ assets/js/categoriasMain.js
@@ -6,16 +6,16 @@ fetch('./assets/data/categorias.json')
     let div = "";    
     data.forEach(function(curso){
         div += `
-        <div id="cardCategory" class=" col-lg-4 mb-lg-0">
-          <div class="card z-index-2 mb-3">           
+        <div id="cardCategory" class=" col-lg-3 mb-lg-0">
+          <div class="bg-white shadow-sm rounded z-index-2 mb-3">           
              <div class="card-body p-0 pb-0 ">
                 <div class=" bg-gradient-primary  pb-2 mb-2">
-                  <div class="-100 ">
-                    <a href="/categorias.html?categoria=${curso.Codigo}"><img src="${curso.imageUrl}" class=" shadow-sm" width="100%" alt=""></a>
+                  <div class="w-100 ">
+                    <a href="/categorias.html?categoria=${curso.Codigo}"><img src="${curso.imageUrl}" class=" " width="100%" alt=""></a>
                   </div>
                 </div>
-                <div class="mx-3">
-                  <a href="${curso.url}"><h6 class="ms-2 mb-0  text-light text-bolder"> ${curso.categoria} </h6></a>
+                <div class="mx-3 text-center">
+                  <a href="${curso.url}"><h5 class="ms-2 mb-0  text-dark text-bolder"> ${curso.categoria} </h5></a>
                   <p class="text-sm ms-2">${curso.tags}</p>
                 </div>
                 <div class="mx-3">

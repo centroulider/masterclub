0ef9ffae21658c2f27b1338320e99a34694decad
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-07-12 23:58:17 -0500

    Update categorias.js

--- assets/js/categorias.js
+++ assets/js/categorias.js
@@ -55,18 +55,18 @@ fetch('../assets/data/superPack.json')
     newArray.forEach(function(curso){
         div += `            
         <div class="p-1 col-12 col-lg-3 p-0 tarjeta">
-            <div class="rounded text-center bg-bluedark">
+            <div class="rounded text-center shadow-sm mb-2">
                 <div class="bg-gradient-primary  pb-1  mb-2">
-                <div class="w-100 p-2">
-                    <a href="https://wa.me/573132047407+*${curso.curso}*+de+${curso.Profesor}"><img src="./assets/img/cursos/${curso.codigo}/${curso.idCurso}.avif" class="shadow-sm" width="100%" alt="${curso.curso}" title="${curso.curso}"></a>
+                <div class="w-100 ">
+                    <a href="https://wa.me/573132047407+*${curso.curso}*+de+${curso.Profesor}"><img src="../assets/img/categorias/${curso.codigo}/${curso.idCurso}.avif" class="shadow-sm" width="100%" alt="${curso.curso}" title="${curso.curso}"></a>
                 </div>
                 </div>
                 <div class="px-3">
                 <div class="div ">
                 
                 <img class=" shadow rounded-2" src="/assets/img/icons/${curso.main}.png" alt="${curso.main}" title="${curso.main}">
-                <a class="text-light text-xxs" href="https://wa.me/+573242793938?text=Hola,%20Master.%20Quiero%20comprar%20el%20curso:%20+*${curso.curso}*+de+${curso.Profesor}"><h6 class="mb-0 text-light ">${curso.curso}</h6></a>
-                <p class="text-xs text-light    ">Nivel: <span class="text-success">${curso.nivel} </span>- <span class="font-weight-bolder ">${curso.duracion} Hs</span></p>
+                <a class="text-xxs" href="https://wa.me/+573242793938?text=Hola,%20Master.%20Quiero%20comprar%20el%20curso:%20+*${curso.curso}*+de+${curso.Profesor}"><h6 class="mb-0  ">${curso.curso}</h6></a>
+                <p class="text-xs     ">Nivel: <span class="text-success">${curso.nivel} </span>- <span class="font-weight-bolder ">${curso.duracion} Hs</span></p>
                 <p class="text-xs ">${curso.Profesor} <span class="font-weight-bolder text-success">${curso.Plataforma}</span></p>
                 </div>
                 <a href="https://wa.me/+573242793938?text=Hola,%20Master.%20Quiero%20comprar%20el%20curso:%20+*${curso.curso}*+de+${curso.Profesor}"><button class="w-100 bg-success mb-3 border-0 rounded text-bolder text-white move-on-hover">Comprar - USD 9.99</button></a>

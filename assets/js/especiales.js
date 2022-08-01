dbcef7955a9263d159e0a0d77e4dc625f5f34d34
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-07-31 22:48:04 -0500

    Update especiales.js

--- assets/js/especiales.js
+++ assets/js/especiales.js
@@ -1,97 +1,91 @@
-var urlPage = window.location.search;
-const urlParams = new URLSearchParams(urlPage);
-const cat = urlParams.get('categoria');
+// Define la fecha límite
+const finPromo = "May 31 2022 23:59:59"
+
+const countDownDate = new Date(finPromo).getTime();
+
+// Actualiza la cuenta cada segundo
+const x = setInterval(function() {
+  const now = new Date().getTime(); // Obtiene la fecha y la hora actual
+  const distance = countDownDate - now;// La diferencia entre la fecha límite y la fecha actual
+  // Cálculos de tiempo para días, horas, minutos y segundos
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Muestra el resultado en los elementos con id definido
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  // Mensaje cuando la cuenta termina
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "OFERTA NO DISPONIBLE";
+  }
+
+  if (distance < 0) {
+    document.getElementById("promocionSuperpack").classList.add("d-none");
+  }
+}, 500);
 
 
-fetch('../assets/data/superPack.json')
-.then(response => response.json() )
-.then(data => {
-    const newArray = data.filter(filtra =>{
-        return filtra.codigo == cat;
-})
-    
 
-    const {codigo, curso, lenguaje, main, nivel, Plataforma, Profesor, duracion, categoria} = newArray[0];
+window.onload=()=> {
+  
+   // Links footer
 
-        const nombreCurso = curso;
-        const nombreLenguaje = lenguaje;
-        const tema = main;
-        const nivelCurso = nivel;
-        const plataforma = Plataforma;
-        const profesor = Profesor;
-        const duracionCurso = duracion;
-        const nombreCategoria = categoria;
-        const codigoCategoria = codigo;
+  const linkmasterclub = document.getElementById("linkMasterClub");
+  linkmasterclub.href = "https://google.com";
 
-    document.getElementById('categoryName').innerHTML = nombreCategoria;
-    
+  const linkpagos = document.getElementById("linkPagos");
+  linkpagos.href = "https://google.com";
 
-    const numCursos = newArray.length;
-    document.getElementById('numCursos').innerHTML = numCursos;
-    
-    
-    class horas extends Array {
-    sum(key){
-          return this.reduce((a, b) => a + (b[key] || 0), 0);
-        }
-      }
+  const linkgarantia = document.getElementById("linkGarantia");
+  linkgarantia.href = "https://google.com";
 
-    const totalDuracion = new horas(...newArray);
-    const totalHoras= totalDuracion.sum('duracion');
+  const linkprivacidad = document.getElementById("linkPrivacidad");
+  linkprivacidad.href = "https://google.com";
 
-     document.getElementById('total_horas').innerHTML = totalHoras;
+  // links ctas
+
+  // const linkpdf = document.getElementById("linkPdf");
+  // linkpdf.href = "https://masterclub30.digital/lista-todos"
+
+  const linkprueba = document.getElementById("linkPrueba");
+  linkprueba.href = "https://masterclub30.digital/prueba-gratis"
+
+  const linkpreciocategoria = document.getElementById("linkPrecioCategoria");
+  linkpreciocategoria.href = "https://wa.me/+573242793938?text=Hola+master+club.+Quiero+comprar+el+acceso+a+la+categoría+de:"
+
+  const linkoferta = document.getElementById("linkOferta");
+  linkoferta.href = "https://masterclub30.digital/superpack";
 
 
-});
+  // Lnks Menu
+
+  const linkMarketing = document.getElementById("linkMd");
+  linkMarketing.href = "/categorias.html?categoria=md"
+
+  const linkVideo = document.getElementById("linkEv");
+  linkVideo.href = "/categorias.html?categoria=ev"
+
+  const linkDiseno = document.getElementById("linkDg");
+  linkDiseno.href = "/categorias.html?categoria=dg"
+
+  const linkIngles = document.getElementById("linkIn");
+  linkIngles.href = "/categorias.html?categoria=in" 
+
+  const linkDw = document.getElementById("linkDw");
+  linkDw.href = "/categorias.html?categoria=dw"
+
+  const linkinicio = document.getElementById("linkInicio");
+  linkinicio.href = "https://masterclub30.digital"
+
+  const linkhome = document.getElementById("linkHome");
+  linkhome.href = "https://masterclub30.digital"
+};
 
 
-fetch('../assets/data/superPack.json')
-.then(response => response.json() )
-.then(datas => {
-    const newArray = datas.filter(filtro =>{
-        return filtro.codigo === cat;
-})
-    let div = "";    
-    newArray.forEach(function(curso){
-        div += `            
-        <div class="p-1 col-12 col-lg-3 p-0 tarjeta">
-            <div class="rounded text-center shadow-sm mb-2">
-                <div class="bg-gradient-primary  pb-1  mb-2">
-                <div class="w-100 ">
-                    <a href="/curso.html?idCurso=${curso.idCurso}"><img src="../assets/img/categorias/${curso.codigo}/${curso.idCurso}.avif" class="shadow-sm" width="100%" alt="${curso.curso}" title="${curso.curso}"></a>
-                </div>
-                </div>
-                <div class="px-3">
-                <div class="div ">
-                
-                <img class=" shadow rounded-2" src="/assets/img/icons/${curso.main}.png" alt="${curso.main}" title="${curso.main}">
-                <a class="text-xxs" href="/curso.html?idCurso=${curso.idCurso}"><h6 class="mb-0  ">${curso.curso}</h6></a>
-                <p class="text-xs     "><span class="text-dark px-2 rounded">${curso.nivel}</span>- <span class="font-weight-bolder ">${curso.duracion} Hs</span></p>
-                <p class="text-xs ">${curso.Profesor} <span class="font-weight-bolder text-success">${curso.Plataforma}</span></p>
-                </div>
-                <div class="d-flex gap-1">
-                <a class="col-6" href="/curso.html?idCurso=${curso.idCurso}"><button class="w-100 border mb-3 border-0 bg-warning text-black-50 rounded  move-on-hover shadow-sm "><small>Ver más</small></button></a>
-                <a class="col-6" href="checkout.html?idCurso=${curso.idCurso}&Curso=${curso.curso}&precio=${curso.precio}"><button class="w-100 bg-success mb-3 border-0 rounded text-white move-on-hover shadow-sm"><small>Comprar</small></button></a>
-                </div>
-                </div>
-            </div>
-        </div>
-        `;
-    });
-    document.getElementById('lista-cursos').innerHTML = div;
 
 
- });
 
-const d = document;
-
-function searchFilters(input, selector) {
-    d.addEventListener("keyup", e => {
-        if (e.target.matches(input)){
-              
-            d.querySelectorAll(selector).forEach(el => (el.textContent.toLowerCase().includes(e.target.value))?el.classList.remove("filtro"):el.classList.add("filtro"));
-        }
-    }) 
-}
-
-searchFilters(".filtro-tarjetas", ".tarjeta");

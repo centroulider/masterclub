2b20dc59b60a5d3a43c99c08b3bfb8108a51a1c7
Masterclub30 <98167507+Masterclub30@users.noreply.github.com>
2022-08-02 14:02:40 -0500

    Update categoriasMain.js

--- assets/js/categoriasMain.js
+++ assets/js/categoriasMain.js
@@ -11,6 +11,12 @@ fetch('./assets/data/categorias.json')
              <div class="card-body p-0 pb-0 ">
                 <div class=" bg-gradient-primary  pb-2 mb-2">
                   <div class="w-100 ">
+                    <div calss=" m-2">
+                      <div class="positon-absolute d-flex">
+                      <div class="col-6 bg-danger px-3 text-center shadow text-bolder text-white"><span>${curso.precio}<span> USD</div>
+                      <div class="col-6 bg-bluedark px-3 text-center shadow text-bolder text-white"><span>${curso.Cantidad}<span> Cursos</div>
+                      </div>
+                    </div>
                     <a href="/categorias.html?categoria=${curso.Codigo}"><img src="${curso.imageUrl}" class=" " width="100%" alt=""></a>
                   </div>
                 </div>
@@ -18,8 +24,9 @@ fetch('./assets/data/categorias.json')
                   <a href="/categorias.html?categoria=${curso.Codigo}"><h5 class="ms-2 mb-0  text-dark text-bolder"> ${curso.categoria} </h5></a>
                   <p class="text-sm ms-2">${curso.tags}</p>
                 </div>
-                <div class="mx-3">
-                  <a href="/categorias.html?categoria=${curso.Codigo}"><button class="w-100 bg-success mb-3 border-0 rounded text-bolder text-white move-on-hover">Ver los ${curso.Cantidad} cursos</button></a>
+                <div class="mx-3 d-flex justify-content-evenly">
+                  <a href="/categorias.html?categoria=${curso.Codigo}"><button class="bg-bluedark mb-3  px-3 mx-2 border-0 rounded text-bolder text-white move-on-hover">Ver Cursos</button></a>
+                  <a href="/categorias.html?categoria=${curso.Codigo}"><button class="bg-success mb-3 px-3 mx-2 border-0 rounded text-bolder text-white move-on-hover">Comprar</button></a>
                 </div>
              </div>
           </div>
